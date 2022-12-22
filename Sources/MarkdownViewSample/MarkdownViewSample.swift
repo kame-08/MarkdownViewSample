b5032a54cda9d31c1ce51661092665a0fc9ca703
Kame2021 <21cm0108@jec.ac.jp>
2022-12-23 01:03:06 +0900

    1.0.6

--- Sources/MarkdownViewSample/MarkdownViewSample.swift
+++ Sources/MarkdownViewSample/MarkdownViewSample.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
-@available(iOS 14.0, *)
+@available(iOS 16.0, *)
 public struct MarkdownViewSample {
     public private(set) var text = "Hello, World!"
 }
 
-@available(iOS 14.0, *)
+@available(iOS 16.0, *)
 public struct MarkdownView: View {
     @State var text = ""
     public init() {
@@ -14,22 +14,18 @@ public struct MarkdownView: View {
     
     public var body: some View {
         if text.prefix(4) == "### "{
-            TextField("Markdown", text: $text)
+            TextField("Markdown", text: $text ,axis: .vertical)
                 .font(.title3)
-//            Text(text)
         } else if text.prefix(3) == "## "{
-            TextField("Markdown", text: $text)
+            TextField("Markdown", text: $text ,axis: .vertical)
                 .font(.title2)
-//            Text(text)
         } else if text.prefix(2) == "# "{
-            TextField("Markdown", text: $text)
-//            Text(text)
+            TextField("Markdown", text: $text ,axis: .vertical)
                 .font(.title)
         } else if text.prefix(8) == "https://" {
-            //            Text(text)
-            TextField("Markdown", text: $text)
-                .foregroundColor(Color.blue)
-            
+            TextField("Markdown", text: $text ,axis: .vertical)
+                .foregroundColor(Color(.link))
+                .underline()
         } else {
             TextField("Markdown", text: $text)
         }
