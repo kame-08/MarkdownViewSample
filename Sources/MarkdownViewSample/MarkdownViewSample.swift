285c9824cee6ba0dae9b366b5d11362c736e6dd5
Kame2021 <21cm0108@jec.ac.jp>
2022-12-23 01:09:55 +0900

    1.0.7

--- Sources/MarkdownViewSample/MarkdownViewSample.swift
+++ Sources/MarkdownViewSample/MarkdownViewSample.swift
@@ -8,26 +8,32 @@ public struct MarkdownViewSample {
 @available(iOS 16.0, *)
 public struct MarkdownView: View {
     @State var text = ""
+    @FocusState private var condition:Bool
     public init() {
-        
+        condition = true
     }
     
     public var body: some View {
         if text.prefix(4) == "### "{
             TextField("Markdown", text: $text ,axis: .vertical)
                 .font(.title3)
+                .focused($condition)
         } else if text.prefix(3) == "## "{
             TextField("Markdown", text: $text ,axis: .vertical)
                 .font(.title2)
+                .focused($condition)
         } else if text.prefix(2) == "# "{
             TextField("Markdown", text: $text ,axis: .vertical)
                 .font(.title)
+                .focused($condition)
         } else if text.prefix(8) == "https://" {
             TextField("Markdown", text: $text ,axis: .vertical)
                 .foregroundColor(Color(.link))
                 .underline()
+                .focused($condition)
         } else {
-            TextField("Markdown", text: $text)
+            TextField("Markdown", text: $text ,axis: .vertical)
+                .focused($condition)
         }
     }
 }
