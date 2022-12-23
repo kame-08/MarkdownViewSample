c9db157f31411ea64a7d502cf84587888b17d0d1
Kame2021 <21cm0108@jec.ac.jp>
2022-12-23 09:46:41 +0900

    1.0.9

--- Sources/MarkdownViewSample/MarkdownViewSample.swift
+++ Sources/MarkdownViewSample/MarkdownViewSample.swift
@@ -8,29 +8,70 @@ public struct MarkdownViewSample {
 @available(iOS 16.0, *)
 public struct MarkdownView: View {
     @State var text = ""
+    @State var type: MarkdownViewStyle = .markdoun
     @FocusState private var condition:Bool
     public init() {
-        condition = true
+        //        condition = true
     }
     
     public var body: some View {
         
-        TextField("Markdown", text: $text ,axis: .vertical)
-            .font(markdownCheck(text))
-        
-    }
-    
-    func markdownCheck(_ text: String) -> Font {
-        if text.prefix(4) == "### " {
-            return Font.title3
-        } else if text.prefix(3) == "## " {
-            return Font.title2
-        } else if text.prefix(2) == "# " {
-            return Font.title
-        } else if text.prefix(8) == "https://" {
-            return Font.body
-        } else {
-            return Font.body
+        switch type {
+        case .vertical:
+            VStack {
+                Markdown(text: $text)
+                textEdit(text: $text)
+            }
+        case .horizontal:
+            HStack {
+                textEdit(text: $text)
+                Markdown(text: $text)
+            }
+        case .textEdit:
+            textEdit(text: $text)
+        case .markdoun:
+            Markdown(text: $text)
         }
     }
 }
+
+@available(iOS 14.0, *)
+func markdownCheck(_ text: String) -> Font {
+    if text.prefix(4) == "### " {
+        return Font.title3
+    } else if text.prefix(3) == "## " {
+        return Font.title2
+    } else if text.prefix(2) == "# " {
+        return Font.title
+    } else if text.prefix(8) == "https://" {
+        return Font.body
+    } else {
+        return Font.body
+    }
+}
+
+enum MarkdownViewStyle {
+    case vertical
+    case horizontal
+    case textEdit
+    case markdoun
+}
+
+@available(iOS 16.0, *)
+struct Markdown: View {
+    @Binding var text:String
+    var body: some View {
+        
+        TextField("Markdown", text: $text ,axis: .vertical)
+            .font(markdownCheck(text))
+    }
+}
+
+@available(iOS 16.0, *)
+struct textEdit: View {
+    @Binding var text:String
+    var body: some View {
+        TextField("Markdown", text: $text ,axis: .vertical)
+    }
+}
+
