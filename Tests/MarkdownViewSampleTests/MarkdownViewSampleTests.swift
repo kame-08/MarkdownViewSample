c0a906015aa867ffc346d18505368f4368c8d946
Kame2021 <21cm0108@jec.ac.jp>
2022-12-23 10:05:35 +0900

    1.0.11

--- Tests/MarkdownViewSampleTests/MarkdownViewSampleTests.swift
+++ Tests/MarkdownViewSampleTests/MarkdownViewSampleTests.swift
@@ -7,6 +7,6 @@ final class MarkdownViewSampleTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MarkdownViewSample(type: .vertical).text, "Hello, World!")
+        XCTAssertEqual(MarkdownViewSample(markdownViewStyle: .vertical).text, "Hello, World!")
     }
 }
